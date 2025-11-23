838492c665564a6b9f7d4eeecbd2c54c806664a2
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-23 03:20:00 -0300

    refactor: :recycle: refactorizar y corregir algunas cosas del enlace expandible del menú

--- src/components/AsideNavigation/MenuExpandableLink.tsx
+++ src/components/AsideNavigation/MenuExpandableLink.tsx
@@ -4,6 +4,8 @@ import { useEffect, useId, useState } from 'preact/hooks'
 import { IconArrowDown, IconHome } from '../Icons'
 import { Icon } from '../Icon'
 
+const checkboxName = 'checkbox-menu-items'
+
 export function MenuExpandableLink ({ item: { Icon: IconItem, name, path }, path: pathname, asideMenuRef }: { item: ItemAside, path: string, asideMenuRef: RefObject<HTMLElement | null> }) {
   const [subItems, setSubItems] = useState<MenuSubItem[]>()
 
@@ -27,7 +29,7 @@ export function MenuExpandableLink ({ item: { Icon: IconItem, name, path }, path
     if (asideMenuRef.current) {
       const links = asideMenuRef.current.querySelectorAll('.expandableLink')
       links.forEach(link => {
-        const checkbox = link.querySelector('input[name=checkbox-aside-items]')
+        const checkbox = link.querySelector(`input[name='${checkboxName}']`)
         if (checkbox instanceof HTMLInputElement) {
           checkbox.checked = false
         }
@@ -39,33 +41,33 @@ export function MenuExpandableLink ({ item: { Icon: IconItem, name, path }, path
   return (
     <article class='expandableLink flex relative min-h-10'>
       <input
-        id={`checkbox-aside-${name}`}
+        id={`checkbox-menu-${name}`}
         type='checkbox'
         hidden
-        name='checkbox-aside-items'
-        class='[&:checked~.dropDown]:h-fit [&:not(:checked)~.dropDown]:h-10 [&:checked~label>div]:rotate-180'
+        name={checkboxName}
+        class='peer'
         onInput={handleInput}
       />
-      <div class='absolute w-full h-10 flex justify-between gap-2 items-center'>
+      <div class='absolute w-full h-10 flex justify-between gap-2 items-center peer-checked:[&>label>div]:rotate-180'>
         <a
           href={path}
           class={`${path === pathname ? 'actualPath bg-selected font-medium' : 'hover:bg-neutral-800'}
             flex-1 max-w-51 h-10 min-h-10 flex items-center gap-4 px-3 rounded-lg cursor-pointer active:bg-neutral-600`
           }
         >
-          <div class='size-6 max-h-full aspect-square overflow-hidden'>
+          <Icon class='size-6 max-h-full aspect-square'>
             <IconItem active={path === pathname} />
-          </div>
+          </Icon>
           <span>{name}</span>
         </a>
         <div class='w-[2px] h-7 bg-neutral-800'></div>
-        <label htmlFor={`checkbox-aside-${name}`} class='aspect-square flex items-center justify-center hover:bg-neutral-800 rounded-lg h-full cursor-pointer'>
-          <Icon>
+        <label htmlFor={`checkbox-menu-${name}`} class='group aspect-square flex items-center justify-center hover:bg-neutral-800 rounded-lg h-full cursor-pointer'>
+          <Icon class='transition-all'>
             <IconArrowDown />
           </Icon>
         </label>
       </div>
-      <div class='dropDown w-full overflow-hidden rounded-lg [transition:height_250ms_ease] [interpolate-size:allow-keywords]'>
+      <div class='dropDown w-full overflow-hidden rounded-lg [transition:height_250ms_ease] [interpolate-size:allow-keywords] h-10 peer-checked:h-fit'>
         <div class='w-full h-10' />
         {
           subItems?.map(sub => (
