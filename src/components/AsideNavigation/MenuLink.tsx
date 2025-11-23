bcfbbbc2af712c5db4d17c3cf34754b3d0268f64
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-23 03:40:54 -0300

    style: :art: formatear MenuLink diferente

--- src/components/AsideNavigation/MenuLink.tsx
+++ src/components/AsideNavigation/MenuLink.tsx
@@ -6,9 +6,7 @@ export const MenuLink = ({ name, link, path, children }: { name: string, link: s
       href={link}
       class={`${link === path ? 'actualPath bg-selected font-medium' : 'hover:bg-neutral-800'} flex flex-1 items-center gap-4 h-10 min-h-10 px-3 rounded-lg cursor-pointer active:bg-neutral-600 active:transition-colors`}
     >
-      <div
-        class='h-6 aspect-square'
-      >
+      <div class='h-6 aspect-square'>
         {children}
       </div>
       <span class='text-sm text-nowrap overflow-ellipsis overflow-hidden'>{name}</span>
