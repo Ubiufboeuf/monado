9d1b7c5dd4b4ddf1cbdf695a216672df78acf21e
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-24 16:58:10 -0300

    feat: :sparkles: añadir fallback de carga en home

--- src/components/Home/HomeMainContent.tsx
+++ src/components/Home/HomeMainContent.tsx
@@ -6,6 +6,7 @@ import type { FC } from 'preact/compat'
 import { HomeVideos } from './HomeVideos'
 import { HomeGuest } from './HomeGuest'
 import { HomeError } from './HomeError'
+import { VideoCardFallback } from '../VideoCardFallback'
 
 export function HomeMainContent () {
   const [isComponentReady, setIsComponentReady] = useState(false)
@@ -35,5 +36,20 @@ export function HomeMainContent () {
     else if (!videos.length && isComponentReady) setMainContent(() => HomeError) // no videos (error)
   }, [videos, isComponentReady])
 
-  return (MainContent && isComponentReady) && <MainContent videos={videos} />
+  // Contenido
+  if (MainContent && isComponentReady) return <MainContent videos={videos} />
+
+  // Fallback
+  return (
+    <div class='grid justify-center items-start ms:gap-4 grid-cols-[min(100%,500px)] ms:grid-cols-[repeat(auto-fill,minmax(312px,1fr))] h-fit w-full max-h-fit max-w-full pt-6 sm:p-6'>
+      <VideoCardFallback />
+      <VideoCardFallback />
+      <VideoCardFallback />
+      <VideoCardFallback />
+      <VideoCardFallback />
+      <VideoCardFallback />
+      <VideoCardFallback />
+      <VideoCardFallback />
+    </div>
+  )
 }
---
+++ src/components/VideoCardFallback.tsx
@@ -0,0 +1,14 @@
+export function VideoCardFallback () {
+  return (
+    <article class='h-full w-full flex flex-col'>
+      <section class='w-full aspect-video bg-neutral-700 xs:rounded-xl' />
+      <section class='w-full flex min-h-29 h-full pt-3 gap-2 flex-1'>
+        <div class='size-9 mr-1 bg-neutral-700 rounded-full' />
+        <div class='flex-1 h-fit'>
+          <div class='w-9/10 h-4 bg-neutral-700 rounded mb-3' />
+          <div class='w-6/10 h-4 bg-neutral-700 rounded' />
+        </div>
+      </section>
+    </article>
+  )
+}
