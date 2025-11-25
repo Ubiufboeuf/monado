a8837b1db163ea7eed4738404e5afcff97e03f9c
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-25 20:46:51 -0300

    feat: :lipstick: mejorar responsive del tab bar

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -16,14 +16,14 @@ export function TabBar () {
   const path = '/'
   
   return (
-    <nav class='fixed bottom-0 flex items-center justify-around h-18 w-full border-t backdrop-blur-2xl bg-base/80 border-neutral-700'>
+    <nav class='fixed bottom-0 flex items-center justify-evenly h-18 w-full sm:px-4 border-t overflow-x-auto [scrollbar-width:none] backdrop-blur-2xl bg-base/80 border-neutral-700'>
       { barLinks.map(({ name, path: link, Icon: LinkIcon }) => (
         <a
           key={uuidv4()}
           href={link}
-          class={`${path === link ? 'active' : ''} group flex flex-col items-center justify-center gap-1 h-full aspect-square`}
+          class={`${path === link ? 'active' : ''} group flex flex-col items-center justify-center gap-1 h-full w-13 mtb:w-17.75`}
         >
-          <div class='flex items-center justify-center h-fit w-fit p-1 px-4 rounded-full text-neutral-300 group-[.active]:bg-neutral-700'>
+          <div class='flex items-center justify-center h-fit w-fit p-1 px-2 mtb:px-4 rounded-full text-neutral-300 group-[.active]:bg-neutral-700'>
             <Icon class='size-6'>
               <LinkIcon active={path === link} />
             </Icon>
