e9c9b6c5821fe2012b42f0f1eb6111cecfb96f54
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-25 18:09:03 -0300

    feat: :lipstick: agregar TabBar
    no está completo, pero al menos visualmente es algo

---
+++ src/components/TabBar.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+import type { ItemAside } from '@/types/env'
+import { IconLibrary, IconHome, IconSearch, IconShorts, IconSubscriptions } from './Icons'
+import { Icon } from './Icon'
+import { v4 as uuidv4 } from 'uuid'
+
+const barLinks: ItemAside[] = [
+  { name: 'Inicio', path: '/', Icon: ({ active = false }) => <IconHome active={active} /> },
+  { name: 'Shorts', path: '/shorts', Icon: ({ active = false }) => <IconShorts active={active} /> },
+  { name: 'Buscar', path: '/search', Icon: ({ active = false }) => <IconSearch active={active} /> },
+  { name: 'Subs', path: '/subs', Icon: ({ active = false }) => <IconSubscriptions active={active} /> },
+  { name: 'Biblioteca', path: '/library', Icon: ({ active = false }) => <IconLibrary active={active} /> }
+]
+
+export function TabBar () {
+  const path = '/'
+  
+  return (
+    <nav class='fixed bottom-0 flex items-center justify-around h-18 w-full border-t backdrop-blur-2xl bg-base/80 border-neutral-700'>
+      { barLinks.map(({ name, path: link, Icon: LinkIcon }) => (
+        <a
+          key={uuidv4()}
+          href={link}
+          class={`${path === link ? 'active' : ''} group flex flex-col items-center justify-center gap-1 h-full aspect-square`}
+        >
+          <div class='flex items-center justify-center h-fit w-fit p-1 px-4 rounded-full text-neutral-300 group-[.active]:bg-neutral-700'>
+            <Icon class='size-6'>
+              <LinkIcon active={path === link} />
+            </Icon>
+          </div>
+          <h1 class='text-xs text-neutral-300 group-[.active]:text-white'>{name}</h1>
+        </a>
+      )) }
+    </nav>    
+  )
+}
