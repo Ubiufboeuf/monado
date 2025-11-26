08028ad9c3bf8eac01b6db2b75c4c6097f486d20
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-26 05:45:16 -0300

    refactor: :label: forzar tipo "const" en los endpoints

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,7 @@ const SERVER_URL = import.meta.env.PUBLIC_SERVER_URL
 export const ENDPOINTS = {
   VIDEOS: `${SERVER_URL}/data/videos?range=0_2`,
   THUMBNAIL: `${SERVER_URL}/poster/`
-}
+} as const
 
 export {
   DEV
