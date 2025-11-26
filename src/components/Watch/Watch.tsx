8737c29d4787d30b308e36852a6deeca5b601714
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-26 05:55:03 -0300

    feat: :sparkles: crear página /watch y componente principal
    el componente es por si hace falta manejar estados o algo de stores, que se puede complicar desde astro

---
+++ src/components/Watch/Watch.tsx
@@ -0,0 +1,31 @@
+import { usePlayerStore } from '@/stores/usePlayerStore'
+import { useEffect } from 'preact/hooks'
+import { getVideo } from '@/services/videoService'
+import { errorHandler } from '@/lib/errors'
+
+export function Watch ({ id }: { id: string }) {
+  const setVideo = usePlayerStore((state) => state.setVideo)
+
+  async function loadVideo () {
+    let video
+    try {
+      video = await getVideo(id)
+    } catch (err) {
+      errorHandler(err, 'Error consiguiendo los datos del video')
+    }
+
+    if (!video) return
+
+    setVideo(video)
+  }
+  
+  useEffect(() => {
+    loadVideo()
+  }, [])
+
+  return (
+    <section class='absolute top-26 xs:top-14 right-0 flex flex-col h-[calc(100%-56px)] min-h-fit w-full sm:w-[calc(100%-72px)] ml:menu-open:w-navbar [transition:width_250ms_ease]'>
+      {/* <Player /> */}
+    </section>
+  )
+}
