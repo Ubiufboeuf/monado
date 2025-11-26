89b70e097fb5c95ab507d622a11bebeb2e4abd24
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-26 05:46:01 -0300

    feat: añadir endpoints para los videos
    para la información y para los streams

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,6 +3,8 @@ const SERVER_URL = import.meta.env.PUBLIC_SERVER_URL
 
 export const ENDPOINTS = {
   VIDEOS: `${SERVER_URL}/data/videos?range=0_2`,
+  VIDEO: `${SERVER_URL}/data/video`,
+  VIDEO_STREAM: `${SERVER_URL}/video`,
   THUMBNAIL: `${SERVER_URL}/poster/`
 } as const
 
