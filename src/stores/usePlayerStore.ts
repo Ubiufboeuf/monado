c9ab0c0e1c0289758999f8df598d4489155fa550
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-26 16:13:03 -0300

    feat: :sparkles: actualizar store y añadir acciones del reproductor

--- src/stores/usePlayerStore.ts
+++ src/stores/usePlayerStore.ts
@@ -1,12 +1,19 @@
+import { playerKeyboardActions } from '@/lib/keyboardActions'
 import type { Video } from '@/types/videoTypes'
 import { create } from 'zustand'
 
 type PlayerStore = {
   video: Video | null
+  player: HTMLVideoElement | null
+  playerActions: typeof playerKeyboardActions
   setVideo: (video: Video) => void
+  setPlayer: (player: HTMLVideoElement) => void
 }
 
 export const usePlayerStore = create<PlayerStore>((set) => ({
   video: null,
-  setVideo: (video) => set({ video })
+  player: null,
+  playerActions: playerKeyboardActions,
+  setVideo: (video) => set({ video }),
+  setPlayer: (player) => set({ player })
 }))
