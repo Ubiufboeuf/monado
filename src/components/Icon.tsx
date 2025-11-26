66898f8253c454bb7e3d6b8f0fd3e5f7f76a4723
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-25 23:11:25 -0300

    refactor: :art: cambiar className por class

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'preact/compat'
 
 export function Icon ({ class: className = '', children }: { class?: string, children: ReactNode }) {
   return (
-    <div className={`${className} not-[[class*=h-]]:h-6 not-[[class*=w-]]:w-6 flex items-center justify-center overflow-hidden`}>
+    <div class={`${className} not-[[class*=h-]]:h-6 not-[[class*=w-]]:w-6 flex items-center justify-center overflow-hidden`}>
       {children}
     </div>
   )
