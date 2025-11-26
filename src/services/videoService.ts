9a3b3b3022225eef99923949e52d6b4d4a2f81cf
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-26 05:52:32 -0300

    fix: arreglar función para formar varios videos
    faltaba agregar el source a los videos

--- src/services/videoService.ts
+++ src/services/videoService.ts
@@ -36,6 +36,37 @@ export async function getVideos (): Promise<Video[]> {
   return videos
 }
 
+export async function getVideo (id: string): Promise<Video | undefined> {
+  let res
+  try {
+    res = await fetch(`${ENDPOINTS.VIDEO}?id=${id}`)
+  } catch (err) {
+    const errorMessage = 'Error con la petición de la información del video'
+    errorHandler(err, errorMessage, 'dev')
+    throw new Error(errorMessage)
+  }
+
+  let data: unknown = undefined
+  try {
+    data = await responseHandler(res)
+  } catch (err) {
+    const errorMessage = 'Error parseando la respuesta de la información'
+    errorHandler(err, errorMessage, 'dev')
+    throw new Error(errorMessage)
+  }
+
+  let video: Video | undefined
+  try {
+    video = formVideo(data as ServerResponse)
+  } catch (err) {
+    const errorMessage = 'Error formando los videos'
+    errorHandler(err, errorMessage, 'dev')
+    throw new Error(errorMessage)
+  }
+  
+  return video
+}
+
 export function formVideos (data: ServerResponse): Video[] {
   if (!data) return []
   
@@ -67,6 +98,7 @@ export function formVideos (data: ServerResponse): Video[] {
       views: v.view_count || -1,
       likes: v.like_count || -1,
       formats: v.formats || [],
+      source: `${ENDPOINTS.VIDEO_STREAM}/${v.id}/manifest.mpd`,
       release_timestamp: v.release_timestamp || -1
     }
 
@@ -74,4 +106,37 @@ export function formVideos (data: ServerResponse): Video[] {
   }
   
   return videos
+}
+
+export function formVideo (data: ServerResponse) {
+  const videoFromServer = data.video
+
+  if (!videoFromServer) {
+    throw new Error('La variable de los datos del video es falsy')
+  }
+  
+  if (!isValidVideo(videoFromServer) || !videoFromServer.id) return
+
+  const assets: VideoAssets = {
+    minThumbnail: `${ENDPOINTS.THUMBNAIL}/${videoFromServer.minimalThumbnail}`,
+    thumbnail: `${ENDPOINTS.THUMBNAIL}/${videoFromServer.thumbnail}`,
+    thumbnails: videoFromServer.thumbnails?.map((t) => ({
+      ...t,
+      url: `${ENDPOINTS.THUMBNAIL}/${t.url}`
+    })) || []
+  }
+  
+  const video: Video = {
+    id: videoFromServer.id,
+    assets,
+    title: videoFromServer.title || 'Título',
+    duration: videoFromServer.duration || -1,
+    views: videoFromServer.view_count || -1,
+    likes: videoFromServer.like_count || -1,
+    formats: videoFromServer.formats || [],
+    source: `${ENDPOINTS.VIDEO_STREAM}/${videoFromServer.id}/manifest.mpd`,
+    release_timestamp: videoFromServer.release_timestamp || -1
+  }
+  
+  return video
 }
