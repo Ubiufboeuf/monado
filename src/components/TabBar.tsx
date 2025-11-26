4903615cee25011005f152c8ab6a607bc35f684e
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-26 07:32:51 -0300

    refactor: invertir visibilidad del MenuMini y agregar opción al TabBar

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -12,11 +12,14 @@ const barLinks: ItemAside[] = [
   { name: 'Biblioteca', path: '/library', Icon: ({ active = false }) => <IconLibrary active={active} /> }
 ]
 
-export function TabBar () {
+export function TabBar ({ hidden }: { hidden?: boolean | undefined }) {
   const path = '/'
   
   return (
-    <nav class='fixed bottom-0 flex desktop:hidden items-center justify-evenly h-tbh w-full sm:px-4 border-t overflow-x-auto [scrollbar-width:none] backdrop-blur-2xl bg-base-dark/80 border-neutral-700'>
+    <nav
+      class='fixed bottom-0 flex desktop:hidden items-center justify-evenly h-tbh w-full sm:px-4 border-t overflow-x-auto [scrollbar-width:none] backdrop-blur-2xl bg-base-dark/80 border-neutral-700'
+      hidden={hidden}
+    >
       { barLinks.map(({ name, path: link, Icon: LinkIcon }) => (
         <a
           key={uuidv4()}
