2491c75b2e44081f8b717680488c43e1dffb8ee5
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-26 16:13:03 -0300

    feat: :sparkles: actualizar store y añadir acciones del reproductor

---
+++ src/lib/keyboardActions.ts
@@ -0,0 +1,19 @@
+import { usePlayerStore } from '@/stores/usePlayerStore'
+
+export const validKeys = [
+  'f'
+]
+
+export const playerKeyboardActions = [
+  { key: 'f', action: toggleFullScreen }
+]
+
+function toggleFullScreen () {
+  const player = usePlayerStore.getState().player
+  if (!player || !document.fullscreenEnabled) return
+
+  const isFullScreen = document.fullscreenElement
+
+  if (isFullScreen) document.exitFullscreen()
+  else player.requestFullscreen()
+}
