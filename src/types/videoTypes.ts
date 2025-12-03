63f2dce5374dffb8e7a5cb38b48dda7c9aa42dd7
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-03 19:33:11 -0300

    feat: :label: actualizar tipos e interfaces de los videos
    es para que coincidan con la nueva estructura de datos que se guardan

--- src/types/videoTypes.ts
+++ src/types/videoTypes.ts
@@ -1,35 +1,58 @@
-export interface Video {
-  id: string
-  title: string
-  duration: number
-  views: number
-  likes: number
-  assets: VideoAssets
-  formats: VideoFormat[]
+export type Video = {
+	id: string
   source: string
-  release_timestamp: number
+	title: string
+	uploader: string
+	uploader_id: string
+	channel_follower_count: number
+	channel_is_verified: boolean
+	duration: number
+	width: number
+	height: number
+	mixed_size: number
+	aspect_ratio: string
+	audios: (AudioMetadata | null)[]
+	resolutions: ResolutionMetadata[]
+	resolutionsById: ResolutionsById
+	min_resolution: string
+	max_resolution: string
+	thumbnails: Thumbnail[]
+	min_thumbnail: string
+	max_thumbnail: string
+	release_timestamp: number
 }
 
-export interface VideoAssets {
-  thumbnails: Thumbnail[]
-  thumbnail: string
-  minThumbnail: string
+export type AudioMetadata = {
+	codec: string
+	codec_long_name: string
+	channel_layout: string | undefined
+	channels: number | undefined
+	bit_rate: number
+	bit_rate_kbps: number
+	duration: number
+	sample_rate: number | undefined
+	size: number | undefined
 }
 
-export interface Thumbnail {
+export type ResolutionMetadata = {
+  id: string
+	codec: string
+	codec_long_name: string
+	bit_rate: number
+	bit_rate_kbps: number
+	duration: number
+	size: number | undefined
+	height: number
+	width: number
+	aspect_ratio: string | undefined
+	fps: number | undefined
+}
+
+export type ResolutionsById = Record<ResolutionMetadata['id'], ResolutionMetadata>
+
+export type Thumbnail = {
   id: string
   height: number
   width: number
-  resolution: `${number}x${number}`
   url: string | null
-}
-
-export interface VideoFormat {
-  duration?: number
-  ext?: string
-  type?: 'audio' | 'video'
-  resolution?: string
-  aspect_ratio?: number
-  height?: number
-  width?: number
 }
