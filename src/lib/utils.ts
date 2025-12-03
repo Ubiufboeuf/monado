4ba3404d0b4d8374efc9a81ca6eefa57a2d5993c
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-03 19:28:59 -0300

    feat: :sparkles: añadir utilidades para conseguir el valor máximo y mínimo de un array

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,33 @@ export function padStart (value: number | string, length: number, fill: string) 
 
   return str.padStart(length, fill)
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function getMax (arr: any[], fallback: 'first' | 'last') {
+  let max = fallback === 'first' ? arr[0] : arr.at(-1)
+
+  const sorted = [...arr].sort((a, b) => {
+    if (a > b) return 1
+    if (a < b) return -1
+    return 0
+  })
+  
+  max = sorted.at(-1) ?? max
+
+  return max
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function getMin (arr: any[], fallback: 'first' | 'last') {
+  let min = fallback === 'first' ? arr[0] : arr.at(-1)
+
+  const sorted = [...arr].sort((a, b) => {
+    if (a > b) return 1
+    if (a < b) return -1
+    return 0
+  })
+  
+  min = sorted.at(0) ?? min
+
+  return min
+}
