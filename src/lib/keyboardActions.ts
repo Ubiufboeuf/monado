0257bbe44c345fa1d5c467d1fc8fc3c714197b69
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-06 19:42:41 -0300

    feat: :sparkles: añadir persistencia de estado del modo cine usando cookies

--- src/lib/keyboardActions.ts
+++ src/lib/keyboardActions.ts
@@ -63,9 +63,12 @@ function changeTime (sec: number) {
 
 function toggleCinemaMode () {
   const html = document.documentElement
-  const inCinemaMode = Boolean(html.dataset.inCinemaMode)
-  if (inCinemaMode) delete html.dataset.inCinemaMode
-  else html.dataset.inCinemaMode = 'true'
+  const inCinemaMode = html.dataset.inCinemaMode === 'true'
+  const newState = !inCinemaMode
+  if (newState) html.dataset.inCinemaMode = 'true'
+  else delete html.dataset.inCinemaMode
+
+  document.cookie = `monado-in-cinema-mode=${newState}; path=/; Secure; SameSite=Strict`
 
   document.exitFullscreen()
     .then(() => delete html.dataset.isFullScreen)
