d2e9c0e90a7647ee50493abfd949063633cde653
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-06 15:00:53 -0300

    feat: añadir utilidades debounce y throttle

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-this-alias */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export function padStart (value: number | string, length: number, fill: string) {
   const str = typeof value === 'number'
     ? value.toString()
@@ -6,7 +8,6 @@ export function padStart (value: number | string, length: number, fill: string) 
   return str.padStart(length, fill)
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function getMax (arr: any[], fallback: 'first' | 'last') {
   let max = fallback === 'first' ? arr[0] : arr.at(-1)
 
@@ -21,7 +22,6 @@ export function getMax (arr: any[], fallback: 'first' | 'last') {
   return max
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function getMin (arr: any[], fallback: 'first' | 'last') {
   let min = fallback === 'first' ? arr[0] : arr.at(-1)
 
@@ -34,4 +34,42 @@ export function getMin (arr: any[], fallback: 'first' | 'last') {
   min = sorted.at(0) ?? min
 
   return min
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+export function debounce<T extends Function> (func: (this: T, ...args: any[]) => any, timeout: number): any {
+  let timeoutId: NodeJS.Timeout | null = null
+
+  return function (this: T, ...args: any[]) {
+    const context = this
+    
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+
+    timeoutId = setTimeout(() => {
+      func.apply(context, args)
+    }, timeout)
+  }
+}
+
+export function throttle (func: (...args: any[]) => any, limit: number) {
+  let inThrottle: boolean
+  let lastResult: any
+
+  return function (this: any, ...args: any[]) {
+    const context = this
+
+    if (!inThrottle) {
+      lastResult = func.apply(context, args)
+      
+      inThrottle = true
+
+      setTimeout(() => {
+        inThrottle = false
+      }, limit)
+    }
+    
+    return lastResult
+  }
 }
