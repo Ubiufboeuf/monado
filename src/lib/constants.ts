4d89ad4043a3f545983e3fd4bccd980759c5c3ea
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-05 23:46:00 -0300

    refactor: aumentar rango para la petición de videos

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ const DEV = import.meta.env.DEV
 const SERVER_URL = import.meta.env.PUBLIC_SERVER_URL
 
 export const ENDPOINTS = {
-  VIDEOS: `${SERVER_URL}/data/videos?range=0_2`,
+  VIDEOS: `${SERVER_URL}/data/videos?range=0_20`,
   VIDEO: `${SERVER_URL}/data/video`,
   VIDEO_STREAM: `${SERVER_URL}/video`,
   THUMBNAIL: `${SERVER_URL}/poster`
