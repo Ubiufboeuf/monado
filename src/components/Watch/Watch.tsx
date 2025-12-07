b85a185592c7f7334e4074c2847599149d1ff7ea
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-07 02:20:49 -0300

    refactor(watch): :lipstick: cambiar elemento para hacer pantalla completa

--- src/components/Watch/Watch.tsx
+++ src/components/Watch/Watch.tsx
@@ -50,8 +50,8 @@ export function Watch ({ id, children }: { id: string, children: ReactNode }) {
   }, [])
 
   return (
-    <section class='absolute top-14 lg:top-17 right-0 grid gap-4 h-fit w-full ml:menu-open:w-navbar lg:not-cinema:px-6 [transition:width_250ms_ease]
-      2xl:max-w-screen-2xl 2xl:left-1/2 2xl:right-[unset] 2xl:-translate-x-1/2
+    <section id='watch' class='absolute top-14 lg:top-17 right-0 grid gap-4 h-fit w-full ml:menu-open:w-navbar lg:not-cinema:px-6 [transition:width_250ms_ease]
+      2xl:max-w-screen-2xl 2xl:left-1/2 2xl:right-[unset] 2xl:-translate-x-1/2 full-screen:overflow-y-scroll bg-base-dark
     
       lg:[grid-template-areas:"player_sugeridos""detalles_sugeridos"]
       lg:grid-cols-[1fr_400px]
@@ -62,6 +62,11 @@ export function Watch ({ id, children }: { id: string, children: ReactNode }) {
 
       cinema:[grid-template-areas:"player_player""detalles_sugeridos"]
       cinema:grid-rows-[74dvh_1fr]
+
+      full-screen:[grid-template-areas:"player_player""detalles_sugeridos"] full-screen:grid-rows-[100dvh_1fr]
+      full-screen:cinema:[grid-template-areas:"player_player""detalles_sugeridos"] full-screen:cinema:grid-rows-[100dvh_1fr]
+
+      full-screen:w-screen full-screen:h-screen
     '>
       <div class='[grid-area:player] overflow-hidden bg-black
         w-full h-fit min-h-fit max-h-full
