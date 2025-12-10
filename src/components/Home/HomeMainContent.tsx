0ad703e58fc7e2723127f653aaec04c9dd80e605
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-10 09:16:08 -0300

    refactor: :lipstick: aumentar cantidad de cards en la carga de videos

--- src/components/Home/HomeMainContent.tsx
+++ src/components/Home/HomeMainContent.tsx
@@ -50,6 +50,10 @@ export function HomeMainContent () {
       <VideoCardFallback />
       <VideoCardFallback />
       <VideoCardFallback />
+      <VideoCardFallback />
+      <VideoCardFallback />
+      <VideoCardFallback />
+      <VideoCardFallback />
     </div>
   )
 }
