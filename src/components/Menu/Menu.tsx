da0b479f106a4da1011f7707b8f8c3cd624effba
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-18 10:35:54 -0300

    feat(ui): :technologist: agregar clases como parámetros a Menu y HideMenu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -4,7 +4,7 @@ import { MenuLink } from '../AsideNavigation/MenuLink'
 import { itemsAside } from '@/lib/menuItems'
 import { useHydrationStore } from '@/stores/useHydrationStore'
 
-export function Menu ({ pathname }: { pathname: string }) {
+export function Menu ({ pathname, class: className }: { pathname: string, class?: string }) {
   const asideMenuRef = useRef<HTMLElement>(null)
   const isMenuOpen = useHydrationStore((state) => state.isMenuOpen)
 
@@ -12,7 +12,7 @@ export function Menu ({ pathname }: { pathname: string }) {
     <aside
       ref={asideMenuRef}
       id='asideMenu'
-      class='group fixed top-14 -left-60 menu-open:left-0 [transition:left_250ms_ease] z-100 w-60 h-[calc(100dvh-56px)] bg-base-dark'
+      class={`${className} group fixed top-14 -left-60 menu-open:left-0 [transition:left_250ms_ease] z-100 w-60 h-[calc(100dvh-56px)] bg-base-dark`}
       aria-hidden={!isMenuOpen}
     >
       <div class='h-full w-full max-h-full flex flex-col items-center gutter-stable px-3 pr-1 pb-2 overflow-y-scroll [scrollbar-width:thin] not-hover:[scrollbar-color:transparent_transparent]'>
