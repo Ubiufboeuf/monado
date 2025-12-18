bb3e29141fce7d05181aa8543a8a8a4894598bf4
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-18 10:35:54 -0300

    feat(ui): :technologist: agregar clases como parámetros a Menu y HideMenu

--- src/components/Menu/HideMenu.tsx
+++ src/components/Menu/HideMenu.tsx
@@ -1,10 +1,10 @@
-export function HideMenu () {
+export function HideMenu ({ class: className }: { class?: string }) {
   return (
     <label
       id='hideMenu'
       role='button'
       htmlFor='checkbox-menu-state'
-      class='fixed left-0 top-0 z-9 w-full h-dvh [transition:all_250ms_ease_allow-discrete] pointer-events-none bg-transparent backdrop-blur-none menu-open:not-ml:pointer-events-auto menu-open:not-ml:bg-black/40 menu-open:not-ml:backdrop-blur-xs'
+      class={`${className} fixed left-0 top-0 z-9 w-full h-dvh [transition:all_250ms_ease_allow-discrete] pointer-events-none bg-transparent backdrop-blur-none menu-open:not-ml:pointer-events-auto menu-open:not-ml:bg-black/40 menu-open:not-ml:backdrop-blur-xs`}
     />
   )
 }
