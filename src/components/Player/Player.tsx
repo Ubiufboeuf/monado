4df83fe3279922622f4db5f36f10f4672694c993
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-20 01:28:42 -0300

    feat(player): crear nuevo Player y custom hook para manejar la lógica de este

---
+++ src/components/Player/Player.tsx
@@ -0,0 +1,51 @@
+import { usePlayer } from '@/hooks/usePlayer'
+import { getPoster } from '@/lib/api'
+import { AUTO_PLAY } from '@/lib/constants'
+import { Icon } from '../Icon'
+import { IconPlay } from '../Icons'
+import type { TargetedEvent } from 'preact'
+
+interface PlayerProps {
+  videoId: string
+  autoplay?: boolean
+  class?: string
+}
+
+export function Player ({ videoId, autoplay = AUTO_PLAY, class: className }: PlayerProps) {
+  const {
+    videoRef,
+    hasPlayed,
+    togglePlayState
+  } = usePlayer()
+
+  function firstPlayVideo (event: TargetedEvent<HTMLButtonElement>) {
+    const button = event.currentTarget
+    button.hidden = true
+    togglePlayState()
+  }
+  
+  return (
+    <div
+      class={`${className} relative`}
+    >
+      <video
+        ref={videoRef}
+        autoPlay={autoplay}
+        poster={getPoster(videoId)}
+        onClick={togglePlayState}
+      />
+      { !hasPlayed && (
+        <button
+          class='absolute left-1/2 top-1/2 -translate-1/2 h-full w-full flex items-center justify-center cursor-pointer outline-0'
+          onClick={firstPlayVideo}
+        >
+          <div class='h-fit w-fit p-1.5 px-4 rounded-2xl [corner-shape:squircle] backdrop-blur pointer-events-none bg-black/80'>
+            <Icon class='size-10'>
+              <IconPlay />
+            </Icon>
+          </div>
+        </button>
+      ) }
+    </div>
+  )
+}
