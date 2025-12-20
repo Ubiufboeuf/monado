e396609f46ed4653c6a83639e5652084082821c7
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-20 10:43:32 -0300

    feat(player): agregar secciones principales de Controls

--- src/components/Player/Controls.tsx
+++ src/components/Player/Controls.tsx
@@ -5,6 +5,7 @@ interface ControlsProps {
 }
 
 export function Controls ({ togglePlayState }: ControlsProps) {
+  const video = usePlayerStore((state) => state.video)
   const controlsVisible = usePlayerStore((state) => state.controlsVisible)
   // const isPlaying = usePlayerStore((state) => state.isPlaying)
 
@@ -28,10 +29,20 @@ export function Controls ({ togglePlayState }: ControlsProps) {
   return (
     <div
       class={`${controlsVisible ? '' : 'hide'} absolute left-0 top-0 flex flex-col justify-between h-full w-full [.hide]:opacity-0 transition-opacity`}
-      onClick={togglePlayState}
       onContextMenu={handleContextMenu}
     >
-
+      <section
+        class='flex-1 max-h-[calc(100%-68px)] w-full'
+        onClick={togglePlayState}
+      >
+        <h1 class='not-full-screen:hidden'>{video?.title}</h1>
+      </section>
+      <section
+        class='relative bottom-0 w-full h-fit px-2'
+      >
+        {/* <CustomRange /> */}
+        {/* Panel inferior */}
+      </section>
     </div>
   )
 }
