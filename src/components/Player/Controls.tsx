d10b2b53601a32e7d2ccd0f34bcb150e9e57692f
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-20 17:26:15 -0300

    refactor(player): mover clases del Timeline a Controls

--- src/components/Player/Controls.tsx
+++ src/components/Player/Controls.tsx
@@ -54,6 +54,10 @@ export function Controls ({ videoRef, hidden }: ControlsProps) {
           maxValue={video?.duration ?? 100}
           listenForProgress={currentTime}
           onValueUpdate={handleTimeUpdate}
+          rangeClass='group absolute bottom-14.5 left-1/2 translate-y-1/2 -translate-x-1/2 flex items-center h-5 w-[calc(100%-24px)] cursor-pointer'
+          trackClass='absolute flex items-center h-1 group-hover:h-1.5 w-full cursor-pointer rounded-full transition-all duration-300 bg-gray-900/60'
+          progressClass='absolute h-1 group-hover:h-1.5 rounded-full transition-[height] duration-300 [background:var(--color-gradient)]'
+          thumbClass='absolute left-0 top-1/2 -translate-1/2 size-3 group-hover:size-5 rounded-full transition-[height,width] duration-300 [background:var(--color-gradient)] shadow-2xl'
         />
         <ControlsBottomPanel />
       </section>
