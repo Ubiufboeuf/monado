0959225c2e375d1d958bf24f86f5858a1834a9a0
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-20 14:44:31 -0300

    feat(player): :sparkles: agregar línea de tiempo a Player

--- src/components/Player/Controls.tsx
+++ src/components/Player/Controls.tsx
@@ -1,11 +1,15 @@
 import { usePlayerStore } from '@/stores/usePlayerStore'
+import { CustomRange as Timeline } from './CustomRange'
+import { sleep } from '@/lib/utils'
+import type { RefObject } from 'preact'
 
 interface ControlsProps {
   togglePlayState: () => void
+  videoRef: RefObject<HTMLVideoElement>
   hidden?: boolean
 }
 
-export function Controls ({ togglePlayState, hidden }: ControlsProps) {
+export function Controls ({ togglePlayState, videoRef, hidden }: ControlsProps) {
   const video = usePlayerStore((state) => state.video)
   const controlsVisible = usePlayerStore((state) => state.controlsVisible)
   // const isPlaying = usePlayerStore((state) => state.isPlaying)
@@ -27,22 +31,38 @@ export function Controls ({ togglePlayState, hidden }: ControlsProps) {
     document.addEventListener('click', restore, { once: true })
   }
   
+  async function handleContextMenuTopSection (event: MouseEvent) {
+    const element = event.currentTarget as HTMLElement
+    element.style.pointerEvents = 'none'
+    await sleep(50)
+    element.style.pointerEvents = 'auto'
+  }
+
+  function handleTimeUpdate (time: number) {
+    if (videoRef.current) videoRef.current.currentTime = time
+  }
+  
   return (
     <div
-      class={`${controlsVisible ? '' : 'hide'} absolute left-0 top-0 flex flex-col justify-between h-full w-full [.hide]:opacity-0 transition-opacity`}
+      class={`${controlsVisible ? '' : ''} absolute left-0 top-0 flex flex-col justify-between h-full w-full [.hide]:opacity-0 transition-opacity`}
       onContextMenu={handleContextMenu}
       hidden={hidden}
     >
       <section
         class='flex-1 max-h-[calc(100%-68px)] w-full'
         onClick={togglePlayState}
+        onContextMenu={handleContextMenuTopSection}
       >
         <h1 class='not-full-screen:hidden'>{video?.title}</h1>
       </section>
       <section
         class='relative bottom-0 w-full h-fit px-2'
       >
-        {/* <CustomRange /> */}
+        <Timeline
+          id='timeline'
+          maxValue={video?.duration ?? 100}
+          onValueUpdate={handleTimeUpdate}
+        />
         {/* Panel inferior */}
       </section>
     </div>
