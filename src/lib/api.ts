71f10ea7c57795bcf2955b6928863739f7365e21
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-19 23:22:49 -0300

    feat(video-card): :technologist: agregar función para conseguir el poster del video
    el poster es la carátula con mayor resolución (no sé si lo llegaré a cambiar, pero por ahora eso es el poster)

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,3 +4,8 @@ export function getThumbnail (videoId: string | undefined, thumbnailId: string |
   if (!videoId || !thumbnailId) return
   return `${ENDPOINTS.VIDEO}/${videoId}/thumbnail/${thumbnailId}`
 }
+
+export function getPoster (videoId: string | undefined) {
+  if (!videoId) return
+  return `${ENDPOINTS.VIDEO}/${videoId}/poster`
+}
