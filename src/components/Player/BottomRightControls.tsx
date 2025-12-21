9ad783364cadfdcf77d4b68e020ef679960648d8
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-12-21 14:58:39 -0300

    refactor(player): :recycle: dividir controles de Player en dos componentes
    iba a quedar demasiado largo si estaba todo en un componente

---
+++ src/components/Player/BottomRightControls.tsx
@@ -0,0 +1,13 @@
+export function BottomRightControls () {
+  return (
+    <>
+      {/* Captions */}
+      {/* Resolution */}
+      {/* Settings */}
+      {/* Chat */}
+      {/* Toggle Mini Player */}
+      {/* Toggle Cinema Mode */}
+      {/* Toggle Fullscreen */}
+    </>
+  )
+}
