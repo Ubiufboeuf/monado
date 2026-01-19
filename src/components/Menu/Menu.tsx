ebdbdfb60727c6268baa1dbaa9d3439284a805dd
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-01-19 13:42:50 -0300

    feat(ux): agregar fondo borroso del menú
    Para indicar los elementos principales y agregar un "botón" para cerrar el menú

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -9,49 +9,57 @@ export function Menu ({ pathname, class: className }: { pathname: string, class?
   const isMenuOpen = useHydrationStore((state) => state.isMenuOpen)
 
   return (
-    <aside
-      ref={asideMenuRef}
-      id='asideMenu'
-      class={`${className} group fixed top-14 -left-60 menu-open:left-0 [transition:left_250ms_ease] z-100 w-60 h-[calc(100dvh-56px)] bg-base-dark`}
-      aria-hidden={!isMenuOpen}
-    >
-      <div class='h-full w-full max-h-full flex flex-col items-center gutter-stable px-3 pr-1 pb-2 overflow-y-scroll [scrollbar-width:thin] not-hover:[scrollbar-color:transparent_transparent]'>
-        { itemsAside.map((item) => (
-          <section key={useId()} class='flex flex-col w-full h-fit py-3 text-sm border-b border-neutral-700'>
-            { item.map((item) => {
-              const { Icon, name, type, path: link } = item
-              return (type === 'expand') ? (
-                <MenuExpandableLink
-                  key={useId()}
-                  item={item}
-                  path={pathname}
-                  asideMenuRef={asideMenuRef}
-                  isMenuOpen={isMenuOpen}
-                />
-              ) : (
-                <MenuLink
-                  key={useId()}
-                  name={name}
-                  link={link}
-                  path={pathname}
-                  isMenuOpen={isMenuOpen}
-                >
-                  <Icon active={link === pathname} />
-                </MenuLink>
-              )
-            }) }
+    <>
+      <aside
+        ref={asideMenuRef}
+        id='asideMenu'
+        class={`${className} group fixed top-14 -left-60 menu-open:left-0 [transition:left_250ms_ease] z-100 w-60 h-[calc(100dvh-56px)] bg-base-dark`}
+        aria-hidden={!isMenuOpen}
+      >
+        <div class='h-full w-full max-h-full flex flex-col items-center gutter-stable px-3 pr-1 pb-2 overflow-y-scroll [scrollbar-width:thin] not-hover:[scrollbar-color:transparent_transparent]'>
+          { itemsAside.map((item) => (
+            <section key={useId()} class='flex flex-col w-full h-fit py-3 text-sm border-b border-neutral-700'>
+              { item.map((item) => {
+                const { Icon, name, type, path: link } = item
+                return (type === 'expand') ? (
+                  <MenuExpandableLink
+                    key={useId()}
+                    item={item}
+                    path={pathname}
+                    asideMenuRef={asideMenuRef}
+                    isMenuOpen={isMenuOpen}
+                  />
+                ) : (
+                  <MenuLink
+                    key={useId()}
+                    name={name}
+                    link={link}
+                    path={pathname}
+                    isMenuOpen={isMenuOpen}
+                  >
+                    <Icon active={link === pathname} />
+                  </MenuLink>
+                )
+              }) }
+            </section>
+          ) )}
+          <section>
+            <a
+              href='/about_this_project'
+              class='cursor-pointer text-neutral-300 hover:text-blue-500 transition-colors'
+              tabIndex={isMenuOpen ? 0 : -1}
+            >
+              Acerca de este proyecto
+            </a>
           </section>
-        ) )}
-        <section>
-          <a
-            href='/about_this_project'
-            class='cursor-pointer text-neutral-300 hover:text-blue-500 transition-colors'
-            tabIndex={isMenuOpen ? 0 : -1}
-          >
-            Acerca de este proyecto
-          </a>
-        </section>
-      </div>
-    </aside>
+        </div>
+      </aside>
+      <label
+        id='hideMenu'
+        role='button'
+        htmlFor='checkbox-menu-state'
+        class='fixed left-0 top-0 z-9 w-full h-dvh [transition:all_250ms_ease_allow-discrete] pointer-events-none bg-transparent backdrop-blur-xs opacity-0 menu-open:not-ml:pointer-events-auto menu-open:not-ml:bg-black/40 menu-open:not-ml:opacity-100'
+      />
+    </>
   )
 }
