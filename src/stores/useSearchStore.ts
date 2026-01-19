a0489590020358d8b4f879a32dddfb6f7a6630f2
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-01-19 19:37:05 -0300

    refactor: refactorizar TabBar y cambiar botón de búsqueda
    Se componetizaron los items del TabBar
    Se cambió el enlace de búsqueda por un botón para evitar cambios de página al buscar

--- src/stores/useSearchStore.ts
+++ src/stores/useSearchStore.ts
@@ -3,9 +3,13 @@ import { create } from 'zustand'
 interface SearchStore {
   query: string | undefined
   setQuery: (query: string) => void
+  isTabBarSearching: boolean
+  setIsTabBarSearching: (isSearching: boolean) => void
 }
 
 export const useSearchStore = create<SearchStore>((set) => ({
   query: undefined,
-  setQuery: (query) => set({ query })
+  setQuery: (query) => set({ query }),
+  isTabBarSearching: false,
+  setIsTabBarSearching: (isTabBarSearching) => set({ isTabBarSearching })
 }))
