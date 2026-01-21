ff020fe55520d8dcd13f2be444856aab2a747c50
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-01-21 14:45:19 -0300

    feat(ui): mejorar página de inicio visualmente y en cuanto a resopnsive

--- src/components/Home/HomeVideos.tsx
+++ src/components/Home/HomeVideos.tsx
@@ -4,7 +4,7 @@ import { VideoCard } from '../VideoCard'
 
 export function HomeVideos ({ videos }: { videos: Video[] }) {
   return (
-    <div class='grid justify-center items-start gap-4 grid-cols-[repeat(auto-fill,minmax(312px,1fr))] h-fit w-full max-h-fit max-w-full overflow-x-hidden sm:px-6 sm:pt-3 md:pt-6 not-desktop:pb-tbh'>
+    <div class='grid justify-center items-start gap-4 grid-cols-[repeat(auto-fill,minmax(312px,1fr))] h-fit w-full max-h-fit max-w-full overflow-x-hidden sm:px-6 sm:pt-3 not-desktop:pb-tbh'>
       { videos.map((video) => <VideoCard key={uuidv4()} video={video} />) }
     </div>
   )
--- src/components/VideoCardFallback.tsx
+++ src/components/VideoCardFallback.tsx
@@ -1,14 +1,16 @@
 export function VideoCardFallback ({ class: className }: { class?: string }) {
   return (
-    <article class={`${className} h-full w-full flex flex-col`}>
-      <section class='w-full aspect-video bg-neutral-700 xs:rounded-xl' />
-      <section class='w-full flex min-h-20 h-full pt-3 gap-2 flex-1'>
-        <div class='size-9 mr-1 not-xs:ml-3 bg-neutral-700 rounded-full' />
-        <div class='flex-1 h-fit pt-0.5'>
-          <div class='w-9/10 h-4 bg-neutral-700 rounded mb-3' />
-          <div class='w-6/10 h-4 bg-neutral-700 rounded' />
-        </div>
-      </section>
+    <article class={`${className} h-full w-full flex flex-col items-center`}>
+      <div class='w-full xs:not-sm:w-[max(80%,var(--breakpoint-xs))] sm:not-md:w-[min(100%,500px)]'>
+        <section class='w-full aspect-video bg-neutral-700 xs:rounded-xl' />
+        <section class='w-full flex h-20 xs:h-24 pt-3 gap-2 flex-1'>
+          <div class='size-9 mr-1 not-xs:ml-3 bg-neutral-700 rounded-full' />
+          <div class='flex-1 h-fit pt-0.5'>
+            <div class='w-9/10 h-4 bg-neutral-700 rounded mb-3' />
+            <div class='w-6/10 h-4 bg-neutral-700 rounded' />
+          </div>
+        </section>
+      </div>
     </article>
   )
 }
