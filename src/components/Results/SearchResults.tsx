426e020ab1af08f326e3ffb53fdccd619e81b584
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-01-25 16:06:33 -0300

    fix(ux): arreglar problemas en los resultados de búsqueda
    Algunas cargas fallaban pidiendo los videos desde el servidor.
    Ahora se piden en el cliente

--- src/components/Results/SearchResults.tsx
+++ src/components/Results/SearchResults.tsx
@@ -1,12 +1,34 @@
 import type { Video } from '@/types/videoTypes'
 import { ResultCard } from './ResultCard'
-import { v4 as uuidv4 } from 'uuid'
+import { useEffect, useState } from 'preact/hooks'
+import { search } from '@/services/searchService'
 
-export function SearchResults ({ results }: { results: Video[] }) {
+export function SearchResults () {
+  const [results, setResults] = useState<Video[]>([])
+  
+  async function loadVideos () {
+    const searchParams = new URL(window.location.href)
+    const query = searchParams.searchParams.get('search') ?? ''
+
+    let videos: Video[] = []
+    
+    try {
+      videos = await search(query)
+    } catch (err) {
+      console.error(`Error buscando "${query}" |`, err)
+    }
+
+    setResults(videos)
+  }
+  
+  useEffect(() => {
+    loadVideos()
+  }, [])
+  
   return (
     <section class='absolute top-17 right-0 flex flex-col gap-4 h-[calc(100%-112px)] min-h-fit w-full sm:w-[calc(100%-72px)] ml:menu-open:w-navbar pb-6 px-6 [transition:width_250ms_ease]'>
       { results.map((v) => (
-        <ResultCard key={uuidv4()} video={v} />
+        <ResultCard key={`search-result-card:${v.id}`} video={v} />
       )) }
     </section>
   )
