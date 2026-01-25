24f481b422b0a6de51f0c27d2e25b184d93992b5
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-01-25 14:18:49 -0300

    feat(ux): mejorar infinite scroll en la página principal
    Se agregó un loader y se cambió el margin de las opciones del observer a un elemento mejor configurado (en estilos)

--- src/components/Home/HomeVideos.tsx
+++ src/components/Home/HomeVideos.tsx
@@ -7,7 +7,7 @@ import { errorHandler } from '@/lib/errors'
 import { useIntersectionObserver } from '@/hooks/useIntersectionObserver'
 
 const options: IntersectionObserverInit = {
-  rootMargin: '0px 0px 60% 0px',
+  rootMargin: '0px 0px 0px 0px',
   threshold: 0
 }
 
@@ -38,9 +38,16 @@ export function HomeVideos () {
   }
   
   return (
-    <div class='grid justify-center items-start gap-4 grid-cols-[repeat(auto-fill,minmax(312px,1fr))] h-fit w-full max-h-fit max-w-full overflow-x-hidden pb-6 sm:px-6 sm:pt-3 not-desktop:pb-tbh [content-visibility:auto]'>
+    <div class='grid justify-center items-start gap-4 grid-cols-[repeat(auto-fill,minmax(312px,1fr))] h-fit w-full max-h-fit max-w-full overflow-hidden pb-6 sm:px-6 sm:pt-3 not-desktop:pb-tbh [content-visibility:auto]'>
       { videos.map((video) => <VideoCard key={`home-video-card:${video.id}`} video={video} />) }
-      <div ref={videosSentinelRef} />
+      <div ref={videosSentinelRef} class='absolute z-100 -bottom-[20dvh] h-dvh w-full' />
+      <div
+        class='col-span-full h-16 not-xs:pb-4 w-full flex justify-center items-center gap-4 text-neutral-200'
+        hidden={!cursor}
+      >
+        <div class='size-8 rounded-full border-4 border-current border-t-transparent animate-spin' />
+        <span>Cargando más videos...</span>
+      </div>
     </div>
   )
 }
