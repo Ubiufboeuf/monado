dfaab6c7d97a90214b4b771513e56215623168e2
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-01-30 23:04:38 -0300

    feat(ui): mejorar visualmente (ui y responsive) /watch
    Se mejoró el diseño de grid, afectando a los componentes Player, Watch y de error de sugerencias

--- src/components/Watch/SuggestedVideos/SuggestedVideosError.tsx
+++ src/components/Watch/SuggestedVideos/SuggestedVideosError.tsx
@@ -1,8 +1,8 @@
 export function SuggestedVideosError () {
   return (
-    <div class='h-fit w-full flex items-center justify-center flex-col gap-4 px-4 pb-32'>
-      <h1 class='text-2xl font-bold'>Hubo un error consiguiendo los videos</h1>
-      <p class='w-md max-w-full text-pretty'>
+    <div class='h-full w-full flex items-center justify-center flex-col gap-4 px-4 not-lg:not-cinema:pb-32 group:h-full'>
+      <h1 class='text-2xl font-bold text-center'>Hubo un error consiguiendo los videos</h1>
+      <p class='w-fit max-w-md text-pretty text-center'>
         Si recargar la página no soluciona el error ponte en contacto con el
         <a href='/' class='text-blue-400'> soporte técnico </a>
         para solucionar el problema.
