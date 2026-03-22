0584cc6181b2ce5cd077d4e161b91b9774eaab8c
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-03-22 11:56:26 -0300

    refactor(lib): cambiar parseViews por formatCompactNumber
    se cambió la implementación a mano por el estandar Intl con NumberFormat()

--- src/components/VideoListCard.tsx
+++ src/components/VideoListCard.tsx
@@ -1,4 +1,4 @@
-import { parseDuration, parseViews } from '@/lib/parsers'
+import { parseDuration, formatCompactNumber } from '@/lib/parsers'
 import type { Video } from '@/types/videoTypes'
 import type { TargetedEvent } from 'preact'
 import { useRef } from 'preact/hooks'
@@ -68,7 +68,7 @@ export function VideoListCard ({ video, class: className }: { video: Video, clas
         <strong class='text-sm font-medium text-start line-clamp-2 pt-0.5 pr-3 text-neutral-100'>{title}</strong>
         <span class='text-xs text-start line-clamp-2 pt-0.5 text-neutral-400'>{uploader}</span>
         <div class='text-xs text-start line-clamp-2 pt-0.5 text-neutral-400'>
-          <span>{parseViews(309000)} vistas</span>
+          <span>{formatCompactNumber(309000)} vistas</span>
           &nbsp;<span>•</span>&nbsp;
           <span>hace 20 días</span>
         </div>
