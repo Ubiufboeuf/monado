d53a071ad054cb3d27d47e534e7101b8fe6d3b13
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-03-22 13:57:12 -0300

    feat(hook): mejorar hook useMediaCheck
    se mejoró el estado inicial

--- src/hooks/useMediaCheck.tsx
+++ src/hooks/useMediaCheck.tsx
@@ -1,14 +1,16 @@
 import { useEffect, useRef, useState } from 'preact/hooks'
 
 export function useMediaCheck (query: string, sub = true): [boolean, () => void] {
-  const [mq, setMQ] = useState(false)
+  const match = (query: string) => window.matchMedia(query)
+
+  const [mq, setMQ] = useState(match(query).matches)
   const mediaQueryRef = useRef<MediaQueryList>()
 
   const handleMQChange = (ev: MediaQueryListEvent) => setMQ(ev.matches)
   const unsub = () => mediaQueryRef.current?.removeEventListener('change', handleMQChange)
   
   useEffect(() => {
-    const mediaQuery = window.matchMedia(query)
+    const mediaQuery = match(query)
 
     setMQ(mediaQuery.matches)
     mediaQueryRef.current = mediaQuery
@@ -17,6 +19,6 @@ export function useMediaCheck (query: string, sub = true): [boolean, () => void]
 
     return unsub
   }, [query])
-  
+
   return [mq, unsub]
 }
