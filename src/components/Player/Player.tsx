de518cb17c809b2858dfeca62470fe3bd86c95c1
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-03-22 23:48:57 -0300

    refactor(player): cambiar z-index del player al base para no ocultarlo

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -11,7 +11,7 @@ export function Player () {
       class='relative w-full h-full desktop:not-cinema:xs:rounded-xl overflow-hidden bg-black'
     >
       <video
-        class='absolute -z-1 h-full w-full'
+        class='absolute h-full w-full'
       />
       <Controls />
     </div>
