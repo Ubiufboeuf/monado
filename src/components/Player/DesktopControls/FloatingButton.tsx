9d7ee1a33a5ac93c6a461dc859c38b2ead370fde
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-03-29 14:18:23 -0300

    feat(player): cambiar onClick de FloatingButton en computadora
    se agregó un stop propagation en base a las props que recibe el componente

--- src/components/Player/DesktopControls/FloatingButton.tsx
+++ src/components/Player/DesktopControls/FloatingButton.tsx
@@ -4,14 +4,23 @@ import type { ReactNode } from 'preact/compat'
 interface Props {
   class?: string
   onClick?: (event: TargetedEvent<HTMLButtonElement>) => void
+  stopPropagation?: boolean
   children?: ReactNode
 }
 
-export function FloatingButton ({ class: className, onClick, children }: Props) {
+export function FloatingButton ({ class: className, onClick, stopPropagation = true, children }: Props) {
+  function handleClick (event: TargetedEvent<HTMLButtonElement>) {
+    if (stopPropagation) {
+      event.stopPropagation()
+    }
+
+    onClick?.(event)
+  }
+
   return (
     <button
       class={`${className} absolute flex items-center justify-center rounded-full cursor-pointer transition-colors shr:bg-neutral-700/70`}
-      onClick={onClick}
+      onClick={handleClick}
     >
       {children}
     </button>
